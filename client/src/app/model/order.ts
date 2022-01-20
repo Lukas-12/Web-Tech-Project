4563986a472aba389fb9d07d925d04cd8dc6c97b
Johannes Ladurner <80148992+JohannesLadurner@users.noreply.github.com>
2022-01-20 23:24:56 +0100

    tabelle in bestellungen hinzugefuegt

--- client/src/app/model/order.ts
+++ client/src/app/model/order.ts
@@ -1,12 +1,12 @@
 import { Item } from './item';
 
 export interface Order {
-  orderid: number; //server
+  orderid: number;
   status: string;
   orderdate: Date;
   tableid: number;
-  paymentreference: string; //server
-  paymenttoken: string; // server
-  totalamount: number;  //server
-  ordereditems: Item[]; //client
+  paymentreference: string;
+  paymenttoken: string;
+  totalamount: number;
+  ordereditems: Item[];
 }
